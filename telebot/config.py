bd9b85efefb16d0cab014fbe4bb0efd467894c4e
Виктория <55043240+zzomg@users.noreply.github.com>
2019-11-14 17:18:06 +0700

    Credentials

---
+++ telebot/config.py
@@ -0,0 +1,5 @@
+# Modify this file to your case
+
+bot_token = '' # bot token (the one you get from @BotFather)
+storage_path = '' # path to directory storing input images
+learner_path = '' # path to directory containing *.pkl export file of your model
